47643a72bcee20b46e1c34bf114d7e349b27fb3d
kgopalrao <kgopalrao@gmail.com>
2013-04-25 18:04:08 -0300

    Update HelloGit.java

--- HelloGit/src/gk/test/HelloGit.java
+++ HelloGit/src/gk/test/HelloGit.java
@@ -7,7 +7,7 @@ public class HelloGit {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello Git 8 with fred and parsu and gopal");
+			System.out.println("Hello Git 8 with fred and parsu and gopal and tim");
 
 	}
 
